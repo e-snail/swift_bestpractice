6a1c3088fa61357a30f6d5fb55a0a41ce42aef80
e-snail <wilbur.k.woo@gmail.com>
2016-05-01 21:09:26 +0800

    hello world

---
+++ SwiftBestPractice/SwiftBestPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftBestPractice
+//
+//  Created by 吴永波 on 16/5/1.
+//  Copyright © 2016年 Wilbur.Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
